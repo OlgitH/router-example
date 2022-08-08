df9fa56b37486f5c37f9884fe2c1f46931f92022
OlgitH <berman.oj@gmail.com>
2022-08-08 10:40:53 +0100

    added router files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./components/pages/home";
+import About from "./components/pages/about";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<Router>
+      <div>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/contact">Contact page</a>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+ 
     </div>
   );
 }
---
+++ src/components/pages/about.js
@@ -0,0 +1,11 @@
+function About() {
+    return (
+        <div>
+            <h1>This is the about page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, a? Commodi accusamus libero assumenda, rerum iure cupiditate, fugiat sequi id quis, molestias reprehenderit accusantium. Laborum aliquam officia amet dolor error?
+            </p>
+        </div>
+    )
+}
+export default About;
